9d1beef7247565153cc1ca5680e2e598f5911ed1
Carlos <carlos-vergikosk@live.com.pt>
2021-08-22 11:40:29 +0200

    asd

--- QuickBuilder.js
+++ QuickBuilder.js
@@ -2,14 +2,13 @@
     'use strict';
    
     $(document).ajaxComplete(function() {
-        console.log("1", document.getElementsByClassName("value_container"));
-        console.log("2", document.getElementsByClassName("value_container")[0]);
-        const selectElement = document.getElementsByClassName("value_container")[0];
+        console.log("2", document.getElementsByClassName("value_container")[1]);
+        const selectElement = document.getElementsByClassName("value_container")[1];
         if (selectElement) {
            selectElement.addEventListener('change', (event) => {
-                const result = document.querySelector('.curr');
-                result.textContent = `You like ${event.target.value}`;
-                console.log("QuickBuilder", result.textContent);
+                const result = selectElement.innerText
+                result = parseInt(result.split(":")[1])
+                console.log("QuickBuilder", result);
             });
         }
     });
