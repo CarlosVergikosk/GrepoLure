b83d7d30a5a76b6333acacfdc39a971891189ba9
Carlos <carlos-vergikosk@live.com.pt>
2021-08-22 11:34:01 +0200

    asd

--- QuickBuilder.js
+++ QuickBuilder.js
@@ -1,13 +1,17 @@
 (function() {
     'use strict';
-    const selectElement = document.getElementsByClassName("value_container")[0];
-    console.log(selectElement)
-    selectElement.addEventListener('change', (event) => {
-
-        const result = document.querySelector('.curr');
-        result.textContent = `You like ${event.target.value}`;
-        console.log("QuickBuilder", result.textContent);
-
+   
+    $(document).ajaxComplete(function() {
+        console.log("1", document.getElementsByClassName("value_container"));
+        console.log("2", document.getElementsByClassName("value_container")[0]);
+        const selectElement = document.getElementsByClassName("value_container")[0];
+        if (selectElement) {
+           selectElement.addEventListener('change', (event) => {
+                const result = document.querySelector('.curr');
+                result.textContent = `You like ${event.target.value}`;
+                console.log("QuickBuilder", result.textContent);
+            });
+        }
     });
 
     console.log("QuickBuilder.js loaded");
